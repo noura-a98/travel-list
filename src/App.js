4f075059b29eff6c9e9b3ed4d5fbfc3b36bb6896
Noura <nouraaaa108@gmail.com>
2025-12-31 21:07:38 +0400

    feat(stats): calculated the stats for the application.

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -117,10 +117,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        {" "}
+        <em>Start adding items to your packing list.</em>
+      </p>
+    );
+  const itemNum = items.length;
+  const itemPacked = items.filter((item) => item.packed).length;
+  const itemPercentage = Math.round((itemPacked / itemNum) * 100);
   return (
     <footer className="stats">
-      <em> 👜 You have X items on your list, and you already packed X (X%) </em>
+      {itemPercentage === 100 ? (
+        "You got everything! Ready to go✈️"
+      ) : (
+        <em>
+          👜 You have {itemNum} items on your list, and you already packed{" "}
+          {itemPacked} (%{itemPercentage}){" "}
+        </em>
+      )}
     </footer>
   );
 }
